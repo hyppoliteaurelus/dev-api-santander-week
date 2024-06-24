81ed27f081463e19179cac9bfc27b9dcb9c436d5
hyppoliteaurelus <haurelus@ymail.com>
2024-06-23 21:41:04 -0300

    commit inicial

---
+++ src/main/java/me/dio/SantanderDevWord2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWord2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWord2024Application.class, args);
+	}
+
+}
